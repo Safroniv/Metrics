08d775350921a43e225b16c12be7ab817393d79d
Иван Сафронов <safroniv@mail.ru>
2022-09-06 14:05:39 +0300

    Домашнее задание ASP.NET Core Web API микросервисы Lesson 2

--- MetricsAgent/Controllers/DotNetErrorsMetricsController.cs
+++ MetricsAgent/Controllers/DotNetErrorsMetricsController.cs
@@ -1,19 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MetricsAgent.Controllers
 {
-    [Route("api/metrics/hdd/left")]
+    [Route("api/metrics/dotnet/errors-count")]
     [ApiController]
-    public class HddLeftMetricsController : ControllerBase
+    public class DotNetErrorsMetricsController : ControllerBase
     {
         /// <summary>
-        /// Получить статистику по свободному месту на жестком диске за период
+        /// Получить статистику по ошибкам в сети DOT.NET за период
         /// </summary>
         /// <param name="fromTime">Время начала периода</param>
         /// <param name="toTime">Время окончания периода</param>
         /// <returns></returns>
         [HttpGet("from/{fromTime}/to/{toTime}")]
-        public IActionResult GetHddLeftMetrics(
+        public IActionResult GetDotNetErrorsMetrics(
             [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
             return Ok();
--- MetricsAgent/Controllers/NetworkMetricsController.cs
+++ MetricsAgent/Controllers/NetworkMetricsController.cs
@@ -3,9 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MetricsAgent.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/metrics/network")]
     [ApiController]
     public class NetworkMetricsController : ControllerBase
     {
+        /// <summary>
+        /// Получить статистику по нагрузке на сеть за период
+        /// </summary>
+        /// <param name="fromTime">Время начала периода</param>
+        /// <param name="toTime">Время окончания периода</param>
+        /// <returns></returns>
+        [HttpGet("from/{fromTime}/to/{toTime}")]
+        public IActionResult GetNetworkMetrics(
+            [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
     }
 }
